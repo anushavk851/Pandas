c4e97a23050f58de0fa6d3a11eeb959d2d2f5e9e
Anusha VK <anushavk851@gmail.com>
2026-09-02 13:02:49 +0530

    Add Series operations in problem1.py

---
+++ Data Structure/Series problems/problem1.py
@@ -0,0 +1,15 @@
+import pandas as pd
+s = pd.Series([25, 40, 15, 60, 35, 80, 50])
+
+#1 Find the number of elements in the Series.
+print("number of elements:",s.size)
+#2 Find the data type of the Series.
+print("datatype:",s.dtype)
+#3 Find the size of the Series.
+print("number of elements:",s.size)
+#4 Find the shape of the Series.
+print("shape:",s.shape)
+#5 Find the index of the Series.
+print("index of series:",s.index)
+#6 Display the values of the Series.
+print("values:",s.values)
